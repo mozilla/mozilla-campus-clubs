e9bf496d6eab0ed57c9d30262917a36a528a6708
Raysa Dutra <dutra.raysa@gmail.com>
2018-10-07 20:33:24 -0300

    add responsiveness and focus for accessibility

--- static/js/css-animations.js
+++ static/js/css-animations.js
@@ -1,6 +1,13 @@
 $('.dropdown-club-corner').mouseenter(function () {
-  $('.dropdown-content').slideToggle();
+    $('.dropdown-content').slideToggle();
 });
+
 $('.dropdown-content').mouseleave(function () {
-  $('.dropdown-content').slideToggle();
+    $('.dropdown-content').slideToggle();
 });
+
+function displayFocus(){
+  var elem = document.getElementsByClassName(".dropdown-club-corner");
+    $('.dropdown-content').slideToggle();
+}
+
